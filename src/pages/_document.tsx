35f062c98c41d965913b9d50cc8bff76728a0f14
MahyaBaghai <mahyabaghai@gmail.com>
2025-10-26 11:54:50 +0330

    add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" suppressHydrationWarning>
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>Golden Coffee</title>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
