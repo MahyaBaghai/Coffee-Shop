ac75b4ec7c491aeb7616ad5eaaed42d535e99c25
MahyaBaghai <mahyabaghai@gmail.com>
2025-10-26 12:07:42 +0330

    update favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en" suppressHydrationWarning>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/Coffee-Shop/favicon.ico" />
         <title>Golden Coffee</title>
       </Head>
       <body className="antialiased">
